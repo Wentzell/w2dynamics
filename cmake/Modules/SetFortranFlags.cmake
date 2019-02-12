4958b06a94c818dc46da5743b88e9e47f700fdaf
Nils Wentzell <nwentzell@flatironinstitute.org>
2019-02-12 12:16:08 -0500

    [cmake] Remove march=native from default fortran flags, Add -fPIC

--- cmake/Modules/SetFortranFlags.cmake
+++ cmake/Modules/SetFortranFlags.cmake
@@ -55,12 +55,7 @@ ENDIF(CMAKE_Fortran_FLAGS_RELEASE AND CMAKE_Fortran_FLAGS_TESTING AND CMAKE_Fort
 #SET_COMPILE_FLAG(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS}"
 #                 Fortran "-fno-underscoring")
 
-# There is some bug where -march=native doesn't work on Mac
-IF(APPLE)
-    SET(GNUNATIVE "-mtune=native")
-ELSE()
-    SET(GNUNATIVE "-march=native")
-ENDIF()
+SET(GNUNATIVE "-fPIC")
 
 #The following does not seem to get added...
 SET_COMPILE_FLAG(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS}"
